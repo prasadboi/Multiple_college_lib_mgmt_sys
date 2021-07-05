a02a20e8fc712adea1076d0fe93a9858494829ae
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2021-07-05 20:27:17 +0530

    removed secret key

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5#xtx^$w2xanp06=(dwk=nkz&i#99jsw^2ir8zygv9$)ema3%c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
